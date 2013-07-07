07913381088550908cbe2d3841d7c30a2360db90
Nam Nguyen <nam@digital-minds.mobi>
2013-07-07 15:20:42 +0700

    handle touches event

--- DemoCocos2D-X/GameLayerScene.cpp
+++ DemoCocos2D-X/GameLayerScene.cpp
@@ -16,6 +16,15 @@ bool GameLayer::init(){
     if(!CCLayer::init()){
         return false;
     }
+    _batchNode = CCSpriteBatchNode::create("hipster.png");
+    this->addChild(_batchNode);
+    CCSpriteFrameCache::sharedSpriteFrameCache()->addSpriteFramesWithFile("hipster.plist");
+    _feeble = CCSprite::createWithSpriteFrameName("r.png");
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    CCLOG("Win size: %0.2f, %0.2f", winSize.width, winSize.height);
+    _feeble->setPosition(ccp(winSize.width/2, winSize.height/2));
+    this->addChild(_feeble);
+    
     this->scheduleUpdate();
     this->setAccelerometerEnabled(true);
     this->setTouchEnabled(true);
@@ -31,15 +40,23 @@ CCScene* GameLayer::scene(){
 }
 void GameLayer::update(float delta)
 {
-    CCLOG("Timer updated");
+//    CCLOG("Timer updated");
 }
 #pragma mark Accelerate
 void GameLayer::didAccelerate(CCAcceleration *pAccelerationValue)
 {
-    CCLOG("Did Accelerate");
+//    CCLOG("Did Accelerate");
 }
 #pragma mark Touches
 void GameLayer::ccTouchesBegan(cocos2d::CCSet *touches, cocos2d::CCEvent *event)
 {
-    CCLOG("Touches began");
+//    CCLOG("Touches began");
+    cocos2d::CCSprite* feeble = CCSprite::createWithSpriteFrameName("r.png");
+    // Choose one of the touches to work with
+    CCTouch* touch = (CCTouch*)( touches->anyObject() );
+    CCPoint location = touch->getLocationInView();
+    location = CCDirector::sharedDirector()->convertToGL(location);
+    
+    feeble->setPosition(location);
+    this->addChild(feeble);
 }
